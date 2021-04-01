538c2f1e8f34cf204c9e66aac46faf645a87162a
Jayshil Patel <78102199+justchilll@users.noreply.github.com>
2021-04-01 15:24:34 +0530

    Add files via upload

---
+++ CODES/Package p1/AdditionClass.java
@@ -0,0 +1,9 @@
+package p1;
+
+public class AdditionClass {
+	public int IWillAdd(int a, int b)
+	{
+		int sum = a + b;
+		return sum;
+	}
+}
---
+++ CODES/Package p1/MyClassA.java
@@ -0,0 +1,12 @@
+package p1; //user defined package
+
+public class MyClassA
+{
+
+		public void ShowMessage()
+		{
+			System.out.println("Hello from Class A");
+			System.out.println("Class A is from package 1");
+		}
+}
+
